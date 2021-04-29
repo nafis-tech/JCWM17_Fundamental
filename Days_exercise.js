1eb3eba255fc4260e309ec4c6be5408610cbaba9
Nafis Laila <75156336+nafis-laila@users.noreply.github.com>
2021-04-29 08:57:34 +0700

    Update Days_exercise.js

--- Days_exercise.js
+++ Days_exercise.js
@@ -1,11 +1,9 @@
-let x = 4,
-    y = 3,
-    z = 2
+let totalHari = 365,
+    tahun = Math.floor(totalHari / 360),
+    sisaTahun = totalHari % 360,
+    bulan = Math.floor(sisaTahun / 30),
+    sisaBulan = sisaTahun % 30,
+    minggu = Math.floor(sisaBulan / 7),
+    hari = sisaBulan % 30
 
-// pangkat **
-let a = (x + y * z) / (x * y)
-let res = a**z
-
-let w = Math.pow(((x + y * z) / (x * y)), z)
-console.log(w)
-console.log(res) 
+console.log(`${tahun} Tahun, ${bulan} Bulan, ${minggu} Minggu, ${hari} Hari`)
