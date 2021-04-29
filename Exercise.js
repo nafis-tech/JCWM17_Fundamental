f3d8c5ae94f742e38fca76ab3361632e2d732e29
Nafis Laila <75156336+nafis-laila@users.noreply.github.com>
2021-04-29 08:20:23 +0700

    Add files via upload

---
+++ Exercise.js
@@ -0,0 +1,19 @@
+//w (x+yxz) / (x x y) pangkat z
+
+let x = 4
+let y = 3
+let z = 2
+let w = (x+y*z)/(x*y))
+console.log(w)
+console.log((Math.pow(w, z))
+
+//akar 3 dai 8
+console.log(Math.cbrt(8))
+
+let totalHari = 485;
+sisa tahun = totalHari%360
+
+
+
+
+
